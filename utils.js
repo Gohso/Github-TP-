74a009f4f73d014adea8c17806de06dc2d2aefb1
hugo tran <hugo77tran@gmail.com>
2025-04-16 16:30:56 +0200

    Correction des erreurs ESLint

--- utils.js
+++ utils.js
@@ -1,18 +1,18 @@
 // utils.js
 
 const sum = (a, b) => {
-    return a + b;
-  };
+  return a + b;
+};
   
-  const multiply = (a, b) => {
-    console.log('Multiplying numbers');
-    return a * b; // Erreur de console.log (dans une règle ESLint par défaut)
-  }
+const multiply = (a, b) => {
+  console.log('Multiplying numbers');
+  return a * b; // Erreur de console.log (dans une règle ESLint par défaut)
+}
   
-  function divide(a, b) {
-    return a / b;
-  }
+function divide(a, b) {
+  return a / b;
+}
   
-  const result = sum(2, 3);
-  multiply(2, 3);
+const result = sum(2, 3);
+multiply(2, 3);
   
